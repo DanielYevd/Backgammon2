1bd288aa28c5b56f42338573f3299b6acafcb45b
danielyevd@gmail.com <danielyevd@gmail.com>
2021-05-10 15:10:11 +0300

    :(

--- BackgammonProject2/ServerData.cs
+++ BackgammonProject2/ServerData.cs
@@ -15,6 +15,7 @@ namespace BackgammonProject2
 {
     class ServerData
     {
+
         public Form1 frm;//הגדרת אובייקט של הטופס שבתוכו יתנהל המשחק/תוכנה
         public Thread tcpThd;//טרנד של הלקוח
         public TcpClient tcpclnt;// מאזין של הלקוח
